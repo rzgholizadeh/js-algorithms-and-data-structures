69e2833ed119dcfd236436e91fc3d08718de081e
Reza <rzgholizadeh@gmail.com>
2020-04-06 18:20:27 -0600

    exercise: Pyramid Printing solution 2

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,7 +14,28 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
+function pyramid(n, row = 0, level = "") {
+	if (row === n) {
+		return;
+	}
+	if (level.length === 2 * n - 1) {
+		console.log(level);
+		pyramid(n, row + 1);
+		return;
+	}
+	const midpoint = Math.floor((2 * n - 1) / 2);
+	let add;
+	if (midpoint - row <= level.length && midpoint + row >= level.length) {
+		add = "#";
+	} else {
+		add = " ";
+	}
+	pyramid(n, row, level + add);
+}
+
+module.exports = pyramid;
+
+function pyramidOne(n) {
 	const midpoint = Math.floor((2 * n - 1) / 2);
 	for (let row = 0; row < n; row++) {
 		let level = "";
@@ -28,5 +49,3 @@ function pyramid(n) {
 		console.log(level);
 	}
 }
-
-module.exports = pyramid;
