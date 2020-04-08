3cc754c28165ad1eb84eaa1cdd6a5d153ebe03e7
Reza <rzgholizadeh@gmail.com>
2020-04-07 19:31:28 -0600

    exercise: Linkedlist - insertLast method

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -33,11 +33,16 @@ class LinkedList {
 	}
 
 	getLast() {
+		if (!this.head) {
+			return null;
+		}
 		let node = this.head;
-		while (node.next) {
+		while (node) {
+			if (!node.next) {
+				return node;
+			}
 			node = node.next;
 		}
-		return node;
 	}
 
 	clear() {
@@ -54,18 +59,27 @@ class LinkedList {
 	removeLast() {
 		if (!this.head) {
 			return;
-        }
-        if(!this.head.next){
-            this.head = null;
-            return;
-        }
-        let previous= this.head;
+		}
+		if (!this.head.next) {
+			this.head = null;
+			return;
+		}
+		let previous = this.head;
 		let node = this.head.next;
 		while (node.next) {
 			previous = node;
 			node = node.next;
 		}
 		previous.next = null;
+	}
+
+	insertLast(data) {
+		const last = this.getLast();
+		if (last) {
+			last.next = new Node(data);
+		} else {
+			this.head = new Node(data);
+		}
 	}
 }
 
